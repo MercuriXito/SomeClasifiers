4f77b9889d990cf89c72ea9a404bdd14a66171d1
victorchen_lab <victor_tochen@foxmail.com>
2019-10-07 14:39:44 +0800

    Add ResNet structures and fix up some bugs in paralized block

--- ClassicalModels.py
+++ ClassicalModels.py
@@ -13,7 +13,7 @@
 
 import torch 
 import torch.nn as nn 
-import torch.functional as F
+import torch.nn.functional as F
 
 ########################################
 # GoogLeNet
@@ -30,27 +30,27 @@ class Inception(nn.Module):
 
         self.paralize1 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels_list[0], 1, 1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
         )
 
         self.paralize2 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels_list[1][0], 1, 1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(out_channels_list[1][0], out_channels_list[1][1], 3, 1, 1),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=False)
         )
 
         self.paralize3 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels_list[2][0], 1, 1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(out_channels_list[2][0], out_channels_list[2][1], 5, 1, 2),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=False)
         )
 
         self.paralize4 = nn.Sequential(
             nn.MaxPool2d(3, 1, 1),
             nn.Conv2d(in_channels, out_channels_list[3], 1, 1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
         )
 
     def forward(self, x):
@@ -123,11 +123,13 @@ class vggblock(nn.Module):
 
         blocks = [
             nn.Conv2d(in_channels, out_channels, 3, 1,1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         ]
 
         for i in range(num_conv - 1):
             blocks.append(nn.Conv2d(out_channels, out_channels, 3, 1, 1))
+            blocks.append(nn.BatchNorm2d(out_channels))
             blocks.append(nn.ReLU(inplace=True))
 
         blocks.append(nn.MaxPool2d(2, 2))
@@ -220,6 +222,288 @@ class NIN(nn.Module):
 
 
 ########################################
+# ResNet
+
+class ResBlock(nn.Module):
+    """Basical residual block
+    """
+    def __init__(self, in_channels, out_channels, stride = 1):
+        super(ResBlock, self).__init__()
+        
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, stride, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+        if in_channels != out_channels: # use 1 * 1 conv as if the size of y changed.
+            self.conv11 = nn.Conv2d(in_channels, out_channels, 1, stride)
+        else:
+            self.conv11 = None
+
+    def forward(self, x):
+        y = self.residual(x)
+        if self.conv11:
+            x = self.conv11(x)
+        return F.relu(x + y, inplace=True)
+
+
+class ResNBlocks(nn.Module):
+    """ Iterally append ResBlock to form a long block.
+    """
+    def __init__(self, in_channels, out_channels, num, stride):
+        super(ResNBlocks, self).__init__()
+        
+        block = [ResBlock(in_channels, out_channels, stride)]
+
+        for i in range(num - 1):
+            block.append(ResBlock(out_channels, out_channels))
+
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        return self.block(x)
+
+###########
+# ResBottleNeck structure
+
+class ResBottleNeck(nn.Module):
+    """ BottleNeck Structure in ResNet50 and ResNet101
+    """
+    def __init__(self, in_channels, out_channels, median_channels, stride = 1):
+        super(ResBottleNeck, self).__init__()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, median_channels, 1, stride), # in bottleneck structure, rescale preformed by the first conv layer.
+            nn.BatchNorm2d(median_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(median_channels, median_channels, 3, 1, 1),
+            nn.BatchNorm2d(median_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(median_channels, out_channels, 1, 1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+        if in_channels != out_channels: # use 1 * 1 conv as if the size of y changed.
+            self.conv11 = nn.Conv2d(in_channels, out_channels, 1, stride)
+        else:
+            self.conv11 = None
+
+    def forward(self, x):
+        y = self.residual(x)
+        if self.conv11:
+            x = self.conv11(x)
+        return F.relu(x + y, inplace=True)
+
+
+class ResNBottleBlocks(nn.Module):
+    """
+    stride 指的是BottleNeck结构的第二层的卷积层的stride，它将减小图片的尺寸至一半，
+    use_11conv 指的是Residual中是否使用1*1的卷积层对x的通道扩展至和卷积层的输出一致，两者基本上没有关系
+    first_stride 指的是串联的一系列的BottleNeck的第一个bottleneck是否作尺寸改变。
+    """
+    def __init__(self, in_channels, out_channels, median_channels, num, first_stride = 2):
+
+        super(ResNBottleBlocks, self).__init__()
+
+        block = [ResBottleNeck(in_channels, out_channels, median_channels, first_stride)]
+        
+        for i in range(num - 1):
+            block.append(ResBottleNeck(out_channels, out_channels, median_channels))
+        
+        self.blocks = nn.Sequential(*block)
+
+    def forward(self, x):
+        return self.blocks(x)
+
+
+class ResNet18(nn.Module):
+
+    def __init__(self, in_channels, out_class):
+        super(ResNet18, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1),
+            ResNBlocks(64, 64, 2, 1),
+            ResNBlocks(64, 128, 2, 2),
+            ResNBlocks(128, 256, 2, 2),
+            ResNBlocks(256, 512, 2, 2)
+        )
+        self.avgpooling = nn.AvgPool2d(7, 1)
+        self.classifier = nn.Sequential(
+            nn.Linear(512, out_class),
+            nn.Softmax(dim = 1)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpooling(x)
+        return self.classifier(x.view(x.size(0), -1))
+
+
+class ResNet34(nn.Module):
+    """ Model of ResNet34, `34` depends on the number of ResBlock in ResNBlocks and the first two layer. 
+        Refer to ResNet18 to compare the differences.
+    """
+    def __init__(self, in_channels, out_class):
+        super(ResNet34, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1),
+            ResNBlocks(64, 64, 3, 1),
+            ResNBlocks(64, 128, 4, 2),
+            ResNBlocks(128, 256, 6, 2),
+            ResNBlocks(256, 512, 3, 2)
+        )
+        self.avgpooling = nn.AvgPool2d(7, 1)
+        self.classifier = nn.Sequential(
+            nn.Linear(512, out_class),
+            nn.Softmax(dim = 1) 
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpooling(x)
+        return self.classifier(x.view(x.size(0), -1))
+
+
+class ResNet50(nn.Module):
+
+    def __init__(self, in_channels, out_class):
+        super(ResNet50, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1),
+            ResNBottleBlocks(64, 256, 64, 3, 1),
+            ResNBottleBlocks(256, 512, 128, 4),
+            ResNBottleBlocks(512, 1024, 256, 6),
+            ResNBottleBlocks(1024, 2048, 512, 3)
+        )
+        self.avgpooling = nn.AvgPool2d(7, 1)
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, out_class),
+            nn.Softmax(dim = 1)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpooling(x)
+        return self.classifier(x.view(x.size(0), -1))
+
+
+########################################
+# ResNet with pre-activation [ modified from
+#  https://github.com/KaimingHe/resnet-1k-layers/blob/master/resnet-pre-act.lua]
+
+class PreABottleNeck(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride):
+        super(PreABottleNeck, self).__init__()
+
+        nPanel = out_channels // 4
+        if in_channels == out_channels:
+            
+            self.increase = False
+            self.blocks = nn.Sequential(
+                nn.BatchNorm2d(in_channels),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(in_channels, nPanel, 1, 1),
+                nn.BatchNorm2d(nPanel),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(nPanel, nPanel, 3, 1, 1),
+                nn.BatchNorm2d(nPanel),
+                nn.ReLU(inplace=False),  
+                nn.Conv2d(nPanel, out_channels, 1, 1),
+            )
+
+
+        else:  # if called for increasing demension
+            self.increase = True            
+            self.activate = nn.Sequential(
+                nn.BatchNorm2d(in_channels),
+                nn.ReLU(inplace=True),
+            )
+
+            self.blocks = nn.Sequential(
+                nn.Conv2d(in_channels, nPanel, 1, stride),
+                nn.BatchNorm2d(nPanel),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(nPanel, nPanel, 3, 1, 1),
+                nn.BatchNorm2d(nPanel),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(nPanel, out_channels, 1, 1)
+            )
+
+            self.shortcut = nn.Conv2d(in_channels, out_channels, 1, stride)
+
+    def forward(self, x):
+        
+        if self.increase:
+            x = self.activate(x)
+            return self.blocks(x) + self.shortcut(x)
+        else:
+            x = self.blocks(x) + x
+
+        return x
+
+
+class PreAResNBottleBolcks(nn.Module):
+
+    def __init__(self, in_channels, out_channels, num, stride):
+        super(PreAResNBottleBolcks, self).__init__()
+
+        # for the first layer
+        blocks = [PreABottleNeck(in_channels, out_channels, stride)]
+
+        for i in range(num - 1): # for the rest layers
+            blocks.append(PreABottleNeck(out_channels, out_channels, 1))
+
+        self.blocks = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        return self.blocks(x)
+
+
+class ResNetPreA(nn.Module):
+    """ResNetN ( (N - 2) % 9 == 0 ) with pre-activation, model structure prepared for cifar10-3*32*32
+    """
+    def __init__(self, in_channels, out_class, depth):
+        super(ResNetPreA, self).__init__()
+
+        assert((depth - 2) % 9 == 0)
+
+        n = (depth - 2) // 9
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 16, 3, 1, 1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            PreAResNBottleBolcks(16, 64, n, 1),
+            PreAResNBottleBolcks(64, 128, n, 2),
+            PreAResNBottleBolcks(128, 256, n, 2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+        self.avgpooling = nn.AvgPool2d(8, 1)
+        self.classifier = nn.Sequential(
+            nn.Linear(256, out_class),
+            nn.Softmax(dim = 1)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpooling(x)
+        return self.classifier(x.view(x.size(0), -1))
+
+
+########################################
 # test defined modules
 
 if __name__ == "__main__":
@@ -227,6 +511,6 @@ if __name__ == "__main__":
     # use tool function in utils to test the structure of models
     import utils as cutils
 
-    for model in [vgg11, NIN, GoogLeNet]:
-        net = model(3, 1000)
-        print("{} output: {}".format(net.__class__.__name__, net(torch.randn([3, 3, 224, 224])).size()))
+    net = ResNetPreA(3, 10, 101 )
+#    cutils.test_model_output(net.features, [32, 3, 32, 32])
+    print(net(torch.randn([32, 3, 32, 32])).size())
